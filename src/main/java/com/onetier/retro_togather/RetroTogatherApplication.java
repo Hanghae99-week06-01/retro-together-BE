94b14bbd0bd3f1b691d8315a11c0a89a9e381485
Puri12 <tkdqja9812@gmail.com>
2022-10-21 16:46:04 +0900

    Init

---
+++ src/main/java/com/onetier/retro_togather/RetroTogatherApplication.java
@@ -0,0 +1,13 @@
+package com.onetier.retro_togather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroTogatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetroTogatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onetier/retro_togather/RetroTogatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onetier.retro_togather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroTogatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
