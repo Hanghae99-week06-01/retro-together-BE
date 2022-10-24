43d0445887b9923cdd984b2b8fef16328ead6b6e
Puri12 <tkdqja9812@gmail.com>
2022-10-24 20:23:47 +0900

    [#13 HashTag] getAllPost 시 태그 표시하도록 변경

--- src/main/java/com/onetier/retro_together/controller/request/GetAllPostResponseDto.java
+++ src/main/java/com/onetier/retro_together/controller/request/GetAllPostResponseDto.java
@@ -1,11 +1,13 @@
 package com.onetier.retro_together.controller.request;
 
+import com.onetier.retro_together.domain.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * GetAllPostResponseDto 추가 2022 - 10 - 23 오후 8시 30분
@@ -18,6 +20,7 @@ public class GetAllPostResponseDto {
     private Long id;
     private String title;
     private String author;
+    private List<Tag> tags;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
