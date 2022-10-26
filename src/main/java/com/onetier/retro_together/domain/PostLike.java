8f8bcb34d16009a35b76936b187e0cba23aeb281
BAEKDOOSAN <doosan0425@gmail.com>
2022-10-26 14:50:11 +0900

    comment 좋아요(게시글/댓글) 기능 커밋 & 푸시 1. LikeController.java 파일에 게시글 좋아요 & 댓글 좋아요 기능 추가 2. LikeRequestDto.java 파일 추가 작성 3. PostCommentLike.java 파일에 "member_id" @JoinColumn 어노테이션 사용해서 테이블 조인, @ManyToOne 어노테이션 사용해서 다대일관계 추가 , "comment_id" @JoinColumn 어노테이션 사용해서 테이블 조인, @ManyToOne 어노테이션 사용해서 다대일 관계 추가 4. PostCommentLikeRepository.java 파일에 JpaRepository 상속받는 형태로 PostCommentLikeRepository interface 구현 커밋 5. PostCommentLikeService.java 파일에 좋아요 및 좋아요 취소 기능 커밋 6. PostLike.java 파일에 "member_id" @JoingColumn 어노테이션 사용해서 테이블 조인, @ManyToOne 어노테이션 사용해서 다대일 관계 추가 , "post_id" @JoinColumn 어노테이션 사용해서 테이블 조인, @ManyToOne 어노테이션 사용해서 다대일 관계 추가 커밋 && 푸시 7. PostLikeRepository.java 파일에 JpaRepository 상속 받는 형태로    PostLikeRepository interface 구현 커밋 8. PostLikeService.java 파일에

---
+++ src/main/java/com/onetier/retro_together/domain/PostLike.java
@@ -0,0 +1,28 @@
+package com.onetier.retro_together.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PostLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @JoinColumn(name = "post_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+}
